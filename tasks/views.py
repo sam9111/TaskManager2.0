2ed5110205862a13d212d6fac3accba9ac6171b0
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-02-02 17:31:19 +0530

    working completed feature

--- tasks/views.py
+++ tasks/views.py
@@ -6,7 +6,7 @@ from tasks.models import Task
 
 def tasks_view(request):
     search_term = request.GET.get("search")
-    tasks = Task.objects.filter(deleted=False)
+    tasks = Task.objects.filter(deleted=False).filter(completed=False)
     if search_term:
         tasks = tasks.filter(title__icontains=search_term)
     return render(request, "tasks.html", {"tasks": tasks})
@@ -25,17 +25,18 @@ def delete_task_view(request, index):
 
 
 def complete_task_view(request, index):
-    completed_tasks.append(tasks[index - 1])
-    del tasks[index - 1]
-    print(completed_tasks)
+    Task.objects.filter(id=index).update(completed=True)
     return HttpResponseRedirect("/completed_tasks/")
 
 
 def completed_tasks_view(request):
+    completed_tasks = Task.objects.filter(deleted=False).filter(completed=True)
     return render(request, "completed_tasks.html", {"completed_tasks": completed_tasks})
 
 
 def all_tasks_view(request):
+    tasks = Task.objects.filter(deleted=False).filter(completed=False)
+    completed_tasks = Task.objects.filter(deleted=False).filter(completed=True)
     return render(
         request, "all_tasks.html", {"tasks": tasks, "completed_tasks": completed_tasks}
     )
