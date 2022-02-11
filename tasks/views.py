f100d159cf2b9c0c481860cc53f9639bf4a3f36e
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-02-11 21:18:04 +0530

    added generic views

--- tasks/views.py
+++ tasks/views.py
@@ -1,27 +1,97 @@
-from django.shortcuts import render
+from ast import Delete
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
 
 from tasks.models import Task
 
 
-def tasks_view(request):
-    search_term = request.GET.get("search")
-    tasks = Task.objects.filter(deleted=False).filter(completed=False)
-    if search_term:
-        tasks = tasks.filter(title__icontains=search_term)
-    return render(request, "tasks.html", {"tasks": tasks})
+class AuthorisedTaskManager(LoginRequiredMixin):
+    def get_queryset(self):
+        return Task.objects.filter(deleted=False, user=self.request.user)
 
 
-def add_task_view(request):
-    task_value = request.GET.get("task")
-    task_obj = Task(title=task_value)
-    task_obj.save()
-    return HttpResponseRedirect("/tasks/")
+class UserLoginView(LoginView):
+    template_name = "user_login.html"
 
 
-def delete_task_view(request, index):
-    Task.objects.filter(id=index).update(deleted=True)
-    return HttpResponseRedirect("/tasks/")
+class UserCreateView(CreateView):
+    form_class = UserCreationForm
+    template_name = "user_create.html"
+    success_url = "/user/login"
+
+
+def session_storage_view(request):
+    total_views = request.session.get("total_views", 0)
+    request.session["total_views"] = total_views + 1
+    return HttpResponse(f"total views: {total_views} {request.user.is_authenticated}")
+
+
+class GenericTaskDeleteView(AuthorisedTaskManager, DeleteView):
+    model = Task
+    template_name = "task_delete.html"
+    success_url = "/tasks"
+
+
+class GenericTaskDetailView(DetailView):
+    model = Task
+    template_name = "task_detail.html"
+
+
+class TaskCreateForm(ModelForm):
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        if len(title) < 10:
+            raise ValidationError("Data too small")
+        return title.upper()
+
+    class Meta:
+
+        model = Task
+        fields = ["title", "description", "completed"]
+
+
+class GenericTaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskCreateForm
+    template_name = "task_update.html"
+    success_url = "/tasks"
+
+
+class GenericTaskCreateView(CreateView):
+    form_class = TaskCreateForm
+    template_name = "task_create.html"
+    success_url = "/tasks"
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.success_url)
+
+
+class GenericTaskView(LoginRequiredMixin, ListView):
+    template_name = "tasks.html"
+    context_object_name = "tasks"
+    paginate_by = 2
+
+    def get_queryset(self):
+        search_term = self.request.GET.get("search")
+        tasks = Task.objects.filter(deleted=False, user=self.request.user).filter(
+            completed=False
+        )
+        if search_term:
+            tasks = tasks.filter(title__icontains=search_term)
+        return tasks
 
 
 def complete_task_view(request, index):
